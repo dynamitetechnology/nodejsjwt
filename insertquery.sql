11c1bc1ff1a6007fd19169a0aea2ffd42981070a
dynamitetechnology <technologydynamite@gmail.com>
2021-06-02 03:37:36 +0530

    firstcommit

---
+++ insertquery.sql
@@ -0,0 +1,21 @@
+INSERT INTO "users" ("id", "name", "email", "password", "phone", "image") VALUES (1, '', '@', '$2b$10$yyuLNtEN/bLVonhSHLatJOnzFx6iAxHYLjWzCeI/V.8I9XZxJ2VZe', '', NULL);
+INSERT INTO "users" ("id", "name", "email", "password", "phone", "image") VALUES (2, 'Sahil', 'sahilw@gmail.com', '$2b$10$TvwT650PTmYM9mOM3zonLeDBABAEVPJAZ6OBm9aJM7h7GAKH8aXGK', '7666321805', NULL);
+INSERT INTO "users" ("id", "name", "email", "password", "phone", "image") VALUES (3, 'Sahil', 'sahil1@gmail.com', '$2b$10$./.gbhfsDsoPUYGHLRmFMutRVsGPCfTExflAD5CnofbV2SC7s.Utm', '7666321805', NULL);
+INSERT INTO "users" ("id", "name", "email", "password", "phone", "image") VALUES (4, 'suraj', 'sahilkumar@gmail.com', '$2b$10$QOkz3G3UGWf5SZBK1ZoJPOnY9gRGZe6hmrl5oI8ktob3pAd1sF8I6', '7666321805', NULL);
+INSERT INTO "users" ("id", "name", "email", "password", "phone", "image") VALUES (5, 'suraj', 'sahilkumar1@gmail.com', '$2b$10$GBdzFuNUBpH2lWLikXtUWeC.1b3zzInnr/ecay6.1l4CgwvSD/xCK', '7666321805', NULL);
+INSERT INTO "users" ("id", "name", "email", "password", "phone", "image") VALUES (6, 'suraj', 'sahilkumar1ww@gmail.com', '$2b$10$CdS4qWbA6wJoDaWzzuFzWu9SESiTTMpe2YzOZ5QSp4kDVIiIl.6rG', '7666321805', 'image_1622576444058.PNG');
+INSERT INTO "users" ("id", "name", "email", "password", "phone", "image") VALUES (7, 'suraj', 'sahilkumar1wwwwww@gmail.com', '$2b$10$hikY7GQnCgVt1ipqYqSSfeQr01LXDsm7d46KXBBRhL/pULNseEDMO', '766632180511', 'image_1622577005578.PNG');
+INSERT INTO "users" ("id", "name", "email", "password", "phone", "image") VALUES (8, 'suraj', 'pawan@gmail.com', '$2b$10$487WlzFAfmAUQs1nMn12We49HF/3aHVzQIbaIawKHsL1ugRpayf.y', '766632180511', 'image_1622577039972.PNG');
+INSERT INTO "users" ("id", "name", "email", "password", "phone", "image") VALUES (9, 'suraj', 'rawan@gmail.com', '$2b$10$j54mzmsv9wuMFfSYgHSX2ecwW9myqKk/AyP7bRljMvRvhBBc3bjva', '766632180511', 'image_1622577177066.PNG');
+INSERT INTO "users" ("id", "name", "email", "password", "phone", "image") VALUES (10, 'Rahul', 'rahul@gmail.com', '$2b$10$41GsPw/oTCs509pjLXuENeujCbLLEv0GAEZducim2OoRYmIV2bhkW', '766632180511', 'image_1622577413776.PNG');
+INSERT INTO "users" ("id", "name", "email", "password", "phone", "image") VALUES (11, 'Kapil', 'Kapil@gmail.com', '$2b$10$NeENqOxy6zehXQ7KAZ5UZukznSUwHo5q5h.j8PEpEGFEwGdT0U20G', '7400401805', 'upload\images\image_1622577512161.jpg');
+INSERT INTO "users" ("id", "name", "email", "password", "phone", "image") VALUES (12, 'Kapil', 'ajay@gmail.com', '$2b$10$59d5vKad73lbZaxDu7BosOBHqAmAnO61ykby0tNGlHVpS/wvV/xYu', '7400401805', 'upload\images\image_1622578403983.jpg');
+
+
+
+
+
+
+INSERT INTO post_collection ("id", "title", "description", "createdby") VALUES (1, 'Hello', 'Hello Dear fullu', 'pawan@gmail.com');
+INSERT INTO post_collection ("id", "title", "description", "createdby") VALUES (2, 'Happy Holi', 'Hello Dear Fullu Happy Holi this is holi description', 'pawan@gmail.com');
+INSERT INTO post_collection ("id", "title", "description", "createdby") VALUES (3, 'What is Lorem Ipsum', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'pawan@gmail.com');
